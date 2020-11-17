675fa041b630db4649b5800031ef4dfd8d21b3cd
Vinayak Kulkarni <inbox.vinayak@gmail.com>
2020-11-17 11:57:06 +0530

    refactor(rollup): add ts support for rollup

--- build/rollup.min.config.js
+++ build/rollup.min.config.js
@@ -2,24 +2,35 @@ import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
 import vue from 'rollup-plugin-vue';
+import alias from '@rollup/plugin-alias';
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from 'rollup-plugin-typescript2';
+
+const extensions = ['.js', '.ts', '.vue'];
 
 export default {
   input: 'src/index.js',
   output: {
     format: 'umd',
-    name: 'vue-tree',
+    name: 'v-tree',
     file: 'dist/v-tree.min.js',
     globals: {
       '@vue/composition-api': 'vueCompositionApi',
     },
   },
   plugins: [
+    alias(),
+    resolve({ extensions, browser: true }),
     babel({
+      babelHelpers: 'bundled',
       exclude: 'node_modules/**',
     }),
     commonjs(),
-    terser(),
-    vue(),
+    vue({ isWebComponent: true, template: { isProduction: true } }),
+    typescript({
+      include: [/\.tsx?$/, /\.vue\?.*?lang=ts/],
+      useTsconfigDeclarationDir: true,
+    }),
   ],
   external: ['@vue/composition-api'],
 };
