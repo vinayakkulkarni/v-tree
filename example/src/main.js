cedf7da611378fcafb6953e759b42c7904b8cced
Vinayak Kulkarni <inbox.vinayak@gmail.com>
2020-11-17 11:57:28 +0530

    style: lintfix example

--- example/src/main.js
+++ example/src/main.js
@@ -1,14 +1,14 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Demo from './Demo.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Demo from './Demo.vue';
 import VTree from '../../src/index';
-import TreeView from '@ll931217/vue-treeview'
+import TreeView from '@ll931217/vue-treeview';
 
-Vue.config.productionTip = false
-Vue.use(VueRouter)
-Vue.use(VTree)
-Vue.use(TreeView)
+Vue.config.productionTip = false;
+Vue.use(VueRouter);
+Vue.use(VTree);
+Vue.use(TreeView);
 
 new Vue({
-  render: h => h(Demo),
-}).$mount('#app')
+  render: (h) => h(Demo),
+}).$mount('#app');
